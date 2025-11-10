77d3ac0f7aff3fb277ee29d53139f48896b5b116
Vemireddy Ravi Kishore Reddy <vemireddyravikishorereddy0504@gmail.com>
2025-11-10 21:12:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,35 +5,34 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
-  build: {
-    outDir: "dist/spa",
-  },
-  plugins: [react(), expressPlugin()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
-    },
-  },
+ base: "/Youtube-Clone/",
+ server: {
+   host: "::",
+   port: 8080,
+   fs: {
+     allow: ["./client", "./shared"],
+     deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
+   },
+ },
+ build: {
+   outDir: "dist/spa",
+ },
+ plugins: [react(), expressPlugin()],
+ resolve: {
+   alias: {
+     "@": path.resolve(__dirname, "./client"),
+     "@shared": path.resolve(__dirname, "./shared"),
+   },
+ },
 }));
 
 function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
-      const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
-      server.middlewares.use(app);
-    },
-  };
+ return {
+   name: "express-plugin",
+   apply: "serve", // Only apply during development (serve mode)
+   configureServer(server) {
+     const app = createServer();
+     server.middlewares.use(app);
+   },
+ };
 }
